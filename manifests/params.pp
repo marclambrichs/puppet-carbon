0e218676b837947ebd76bff50b83f184a4ef98e6
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-03-19 06:45:19 +0100

    Added os case for versioning packages Adapted regex for majrelease

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,26 @@
 #
 #
 class carbon::params {
-  $gr_carbon_ver   = '0.9.15'
   $gr_carbon_pkg   = 'python-carbon'
-  $gr_twisted_ver  = '12.2.0'
   $gr_twisted_pkg  = 'python-twisted-core'
-  $gr_whisper_ver  = '0.9.15'
   $gr_whisper_pkg  = 'python-whisper'
   $manage_packages = true
+
+  case $::osfamily {
+    'RedHat', 'CentOS': {
+      case $::operatingsystemmajrelease {
+        /^7$/: {
+          $gr_carbon_ver   = '0.9.15-1.el7'
+          $gr_twisted_ver  = '12.2.0-4.el7'
+          $gr_whisper_ver  = '0.9.15-1.el7'
+        }
+        default: {
+          fail("unsupported RedHat major release: ${::operatingsystemmajrelease}")
+        }
+      }
+    }
+    default: {
+      fail("unsupported osfamily: ${::osfamily}.")
+    }
+  }
 }
