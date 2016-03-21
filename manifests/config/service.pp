cebf1f9babee52c0f5887de0d56d41ab554c04e2
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-03-21 08:17:46 +0100

    Changed rwx of service files

--- manifests/config/service.pp
+++ manifests/config/service.pp
@@ -4,7 +4,7 @@ define carbon::config::service () {
     ensure  => file,
     content => template('carbon/usr/lib/systemd/system/carbon-cache.service.erb'),
     group   => 'root',
-    mode    => '0755',
+    mode    => '0644',
     owner   => 'root',
     path    => "/usr/lib/systemd/system/carbon-cache-${title}.service",
   }
